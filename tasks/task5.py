996495fee0716a5099ebc828f482e86e1add2907
Ильдар Мазунин <sklockenglish@gmail.com>
2025-10-21 02:43:30 +0500

    привет

--- tasks/task5.py
+++ tasks/task5.py
@@ -2,8 +2,14 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
-   
+    a = input()
+
+    b = a.split()
+
+    for symbol in b:
+        c = ord(symbol)
+        print(f"Код символа {symbol} равен {c}")
+        
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
