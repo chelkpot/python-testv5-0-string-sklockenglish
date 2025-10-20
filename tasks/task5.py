dd035aad4d15d19f89a6da3153931f61ee4e36b5
sklockenglish <sklockenglish@gmail.com>
2025-10-21 03:33:18 +0500

    Update task5.py

--- tasks/task5.py
+++ tasks/task5.py
@@ -9,10 +9,7 @@ def solve():
     for symbol in b:
         c = ord(symbol)
         print(f"Код символа {symbol} равен {c}")
-<<<<<<< HEAD
-   
-=======
-        
+  
 >>>>>>> 996495fee0716a5099ebc828f482e86e1add2907
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
